c8ca91a9eb447822354b54ae6b4ccd56e6d2e2a0
JakeFC <jacobchavera@yahoo.com>
2021-06-21 20:51:16 +0000

    solved if logic and attribute sorting in jinja

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 import operator
 from models import storage
 from models.state import State
+from os import getenv
 
 app = Flask(__name__)
 
@@ -21,7 +22,7 @@ def cities_by_states():
     states = sorted(storage.all(State).values(), key=operator.attrgetter
                     ('name'))
     return render_template('8-cities_by_states.html', states=states,
-                           storage=storage.__class__.__name__)
+                           storageT=getenv('HBNB_TYPE_STORAGE'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
