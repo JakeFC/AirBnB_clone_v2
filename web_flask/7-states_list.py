077e13e144a417c72a00aee449ae58fdbf169511
JakeFC <jacobchavera@yahoo.com>
2021-06-21 18:10:23 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,14 +2,15 @@
 """script that starts a Flask web application"""
 
 from flask import Flask, render_template
+import operator
 from models import storage
-from models.states import State
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception=None):
     """removes the current SQLAlchemy Session"""
     storage.close()
 
@@ -17,7 +18,8 @@ def teardown():
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """returns a dynamic web page for '/states_list' route"""
-    return render_template('7-states_list.html', states=storage.all(State))
+    return render_template('7-states_list.html', states=sorted(storage.all
+                           (State).values(), key=operator.attrgetter('name')))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
